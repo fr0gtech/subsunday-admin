28375f456d9c122cf41ad76af185d5eca4fbdea8
GREEB <gr44bli@gmail.com>
2026-02-06 20:48:55 +0100

    fix: disable auth use CF access control

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,12 +3,14 @@ import type { LayoutServerLoad } from "./$types"
 import { env } from "$env/dynamic/private";
 
 export const load: LayoutServerLoad = async (event) => {
-    if (!event.url.pathname.startsWith('/noaccess')) {
-      const session = await event.locals.auth();
-      if (session?.user?.email !== env.ADMIN_EMAIL) redirect(307, '/noaccess');
-      return {
-        session
-      }
-    }
+  // fuck auth we just use cf access control....
+  // if (!event.url.pathname.startsWith('/noaccess')) {
+  //   const session = await event.locals.auth();
+  //     if (session?.user?.email !== env.ADMIN_EMAIL) redirect(307, '/noaccess');
+  //     return {
+  //       session
+  //     }
+  //   }
+
 
 }
